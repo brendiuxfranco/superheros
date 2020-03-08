64663f36b72e9027b87dbe9d59a1402066130814
brendiuxfranco <60198571+brendiuxfranco@users.noreply.github.com>
2020-03-07 18:18:26 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-teal-800 min-h-screen">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+}
+</script>
