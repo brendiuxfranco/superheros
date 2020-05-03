4b7a9838ccd40c4e10b2b4972cfd652ddc012327
brendiuxfranco <60198571+brendiuxfranco@users.noreply.github.com>
2020-05-02 20:29:54 -0500

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <router-link to="/" class="flex items-center flex-shrink-0 text-white mr-6">
       <img class="h-8 mr-4" src="favicon.png" alt="Logo" />
       <span class="font-semibold text-xl tracking-tight"
-        >SuperHero<span class="font-light">Finder</span></span
+        >Look for a Super<span class="font-light">Hero</span></span
       >
     </router-link>
   </nav>
